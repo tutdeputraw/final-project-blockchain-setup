43f200b5a99571ca948c4d2b16460063d6eac876
Ubuntu <ubuntu@vm-2-property-agent.europe-west1-b.c.hyperledger-fabric-multi-host.internal>
2023-02-20 17:07:05 +0000

    change the orderer ip

--- setup1/vm2/installAndApproveChaincode.sh
+++ setup1/vm2/installAndApproveChaincode.sh
@@ -71,7 +71,7 @@ approveForMyOrg2() {
     setGlobalsForPeer0Org2
 
     # Replace localhost with your orderer's vm IP address
-    peer lifecycle chaincode approveformyorg -o localhost:7050 \
+    peer lifecycle chaincode approveformyorg -o $VM_4:7050 \
         --ordererTLSHostnameOverride orderer.example.com --tls $CORE_PEER_TLS_ENABLED \
         --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} \
         --version ${VERSION} --init-required --package-id ${PACKAGE_ID} \
