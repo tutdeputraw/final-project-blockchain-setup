a0637bd45c05b743aea1abf3e1eac53c33464bb0
root <root@vm-2-property-agent.northamerica-northeast1-a.c.ferrous-quest-382923.internal>
2023-04-18 13:52:20 +0000

    api server

---
+++ setup1/vm1/api-server/chaincode-sdk/queryHelper.js
@@ -0,0 +1,41 @@
+const helper = require('./helper');
+const { Wallets, Gateway } = require('fabric-network');
+
+const QueryHelper = async (org, user, contractName, functionName, functionArgs) => {
+    try {
+        const ccp = helper.GetCCPHelper(org);
+        const walletPath = helper.GetWalletPathHelper(org);
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+
+        console.log(`Wallet path: ${walletPath}`);
+
+        const identity = await wallet.get(user);
+        if (!identity) {
+            console.log(`An identity for the user "${user}" does not exist in the wallet`);
+            return `An identity for the user "${user}" does not exist in the wallet`;
+        }
+
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: user, discovery: { enabled: true, asLocalhost: false } });
+
+        const network = await gateway.getNetwork('mychannel');
+
+        const contract = network.getContract(contractName);
+
+        const result = await contract.evaluateTransaction(functionName, ...functionArgs);
+
+        await gateway.disconnect();
+
+        return helper.responseSuccess(
+            `Transaction has been evaluated`,
+            JSON.parse(result.toString())
+        );
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        return `Failed to evaluate transaction: ${error}`;
+    }
+}
+
+module.exports = {
+    QueryHelper,
+}
