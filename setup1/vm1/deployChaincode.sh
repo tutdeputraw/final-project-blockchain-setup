0c092b09e0b034518a685e8739fee7a499cd90c5
root <root@vm-1-government.us-west4-b.c.ferrous-quest-382923.internal>
2023-04-18 13:51:00 +0000

    change the script

--- setup1/vm1/deployChaincode.sh
+++ setup1/vm1/deployChaincode.sh
@@ -1,11 +1,26 @@
+while getopts v:n: flag
+do
+    case "${flag}" in
+        v)    VERSION=${OPTARG};;
+        n)    CC_NAME=${OPTARG};;
+    esac
+done
+
+CC_RUNTIME_LANGUAGE="golang"
+CC_SRC_PATH=${PWD}/../../artifacts/chaincodes/$CC_NAME
+
+echo "Chaincode";
+echo "Version  : $VERSION";
+echo "Name     : $CC_NAME";
+echo "Path     : $CC_SRC_PATH";
+
+
 export CORE_PEER_TLS_ENABLED=true
 export ORDERER_CA=${PWD}/../vm4/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 export PEER0_ORG1_CA=${PWD}/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export PEER0_ORG2_CA=${PWD}/../vm2/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export PEER0_ORG3_CA=${PWD}/../vm3/crypto-config/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
 export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
-
-
 export CHANNEL_NAME=mychannel
 
 setGlobalsForPeer0Org1() {
@@ -20,39 +35,16 @@ setGlobalsForPeer1Org1() {
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
     export CORE_PEER_ADDRESS=localhost:8051
-
 }
-
-# setGlobalsForPeer0Org2() {
-#     export CORE_PEER_LOCALMSPID="Org2MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-#     export CORE_PEER_ADDRESS=localhost:9051
-
-# }
-
-# setGlobalsForPeer1Org2() {
-#     export CORE_PEER_LOCALMSPID="Org2MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-#     export CORE_PEER_ADDRESS=localhost:10051
-
-# }
 
 presetup() {
     echo Vendoring Go dependencies ...
-    pushd ./../../artifacts/src/github.com/fabcar/go
+    pushd $CC_SRC_PATH
     GO111MODULE=on go mod vendor
     popd
     echo Finished vendoring Go dependencies
 }
 # presetup
-
-CHANNEL_NAME="mychannel"
-CC_RUNTIME_LANGUAGE="golang"
-VERSION="1"
-CC_SRC_PATH="./../../artifacts/src/github.com/fabcar/go"
-CC_NAME="fabcar"
 
 packageChaincode() {
     rm -rf ${CC_NAME}.tar.gz
@@ -70,7 +62,6 @@ installChaincode() {
     echo "===================== Chaincode is installed on peer0.org1 ===================== "
 
 }
-
 # installChaincode
 
 queryInstalled() {
@@ -81,14 +72,13 @@ queryInstalled() {
     echo PackageID is ${PACKAGE_ID}
     echo "===================== Query installed successful on peer0.org1 on channel ===================== "
 }
-
 # queryInstalled
 
 approveForMyOrg1() {
     setGlobalsForPeer0Org1
     # set -x
     # Replace localhost with your orderer's vm IP address
-    peer lifecycle chaincode approveformyorg -o localhost:7050 \
+    peer lifecycle chaincode approveformyorg -o $VM_4:7050 \
         --ordererTLSHostnameOverride orderer.example.com --tls \
         --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
         --init-required --package-id ${PACKAGE_ID} \
@@ -98,7 +88,6 @@ approveForMyOrg1() {
     echo "===================== chaincode approved from org 1 ===================== "
 
 }
-
 # queryInstalled
 # approveForMyOrg1
 
@@ -109,12 +98,11 @@ checkCommitReadyness() {
         --sequence ${VERSION} --output json --init-required
     echo "===================== checking commit readyness from org 1 ===================== "
 }
-
 # checkCommitReadyness
 
 commitChaincodeDefination() {
     setGlobalsForPeer0Org1
-    peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+    peer lifecycle chaincode commit -o $VM_4:7050 --ordererTLSHostnameOverride orderer.example.com \
         --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
         --channelID $CHANNEL_NAME --name ${CC_NAME} \
         --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
@@ -122,7 +110,6 @@ commitChaincodeDefination() {
         --peerAddresses localhost:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
         --version ${VERSION} --sequence ${VERSION} --init-required
 }
-
 # commitChaincodeDefination
 
 queryCommitted() {
@@ -130,30 +117,25 @@ queryCommitted() {
     peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME --name ${CC_NAME}
 
 }
-
 # queryCommitted
 
 chaincodeInvokeInit() {
     setGlobalsForPeer0Org1
-    peer chaincode invoke -o localhost:7050 \
+    peer chaincode invoke -o $VM_4:7050 \
         --ordererTLSHostnameOverride orderer.example.com \
         --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
         -C $CHANNEL_NAME -n ${CC_NAME} \
         --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
          --peerAddresses localhost:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
         --isInit -c '{"Args":[]}'
-
 }
-
- # --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
-
 # chaincodeInvokeInit
 
 chaincodeInvoke() {
     setGlobalsForPeer0Org1
 
     ## Create Car
-    peer chaincode invoke -o localhost:7050 \
+    peer chaincode invoke -o $VM_4:7050 \
         --ordererTLSHostnameOverride orderer.example.com \
         --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
         -C $CHANNEL_NAME -n ${CC_NAME} \
@@ -173,7 +155,6 @@ chaincodeInvoke() {
     #     -c '{"function": "initLedger","Args":[]}'
 
 }
-
 # chaincodeInvoke
 
 chaincodeQuery() {
@@ -187,13 +168,13 @@ chaincodeQuery() {
 # chaincodeQuery
 
 # Run this function if you add any new dependency in chaincode
-# presetup
+presetup
 
-# packageChaincode
-# installChaincode
-# queryInstalled
-# approveForMyOrg1
-# checkCommitReadyness
+packageChaincode
+installChaincode
+queryInstalled
+approveForMyOrg1
+checkCommitReadyness
 # approveForMyOrg2
 # checkCommitReadyness
 # commitChaincodeDefination
